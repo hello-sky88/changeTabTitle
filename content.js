85ad324f7dcf3d8bba0ed791480206222be910e4
Sky Pousti <97752907+hello-sky88@users.noreply.github.com>
2025-11-05 18:50:48 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 document.title = "Britannica school"
 
-let link = document.querySelector("link~=[rel='icon']");
+let link = document.querySelector("link[rel~='icon']");
 
 if (!link) {
   link = document.createElement("link");
   link.rel = "icon";
-  document.getElementsByTagName("head").appendChild(link);
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
 
 link.href = chrome.runtime.getURL("britannicaLogo.png");
